586da575abfb8ac74a91db6d807689a32aa4604f
Vladyslav <vladiskliukin@gmail.com>
2026-02-23 22:27:25 +0200

    first commit

---
+++ src/main/java/dev/vladis/TuneWaveApp/TuneWaveAppApplication.java
@@ -0,0 +1,13 @@
+package dev.vladis.TuneWaveApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuneWaveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuneWaveAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vladis/TuneWaveApp/TuneWaveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vladis.TuneWaveApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuneWaveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
